96840bc3e0b8c8a386f0decce3327e2e938123c8
cmis <cmis@cmis-hs11.(none)>
2015-08-14 14:31:06 +0700

    Created Helloworld

---
+++ Helloworld/Helloworld.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Helloworld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Helloworld
+{
+     public static void main ( String args[] ) // method signature: Consists of modifiers (public, static, void) and a method name (main). 
+  {
+    System.out.println("Hello,World!");
+  } // end main
+} // end HelloWorld
+
