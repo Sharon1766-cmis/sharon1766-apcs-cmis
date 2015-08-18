607862466650c29c142ea6c93fec38daccea39f0
cmis <cmis@cmis-hs10.(none)>
2015-08-18 09:55:19 +0700

    renamed files

---
+++ Helloworld/Nameandage.java
@@ -0,0 +1,25 @@
+  
+
+import java.util.Scanner;
+
+public class Nameandage {
+    
+    public static void main (String [] args) {
+        
+        Scanner user_input = new Scanner (System.in);
+        
+        String first_name;
+        System.out.print("What is your name?");
+        first_name = user_input.next ( ) ;
+        
+        String age;
+        System.out.print ("What is your age?");
+        age = user_input.next ( ) ;
+        
+        System.out.println ("Hello " +first_name+  ",you are " +age+ " years old " );
+        
+    }
+    
+}    
+        
+        
