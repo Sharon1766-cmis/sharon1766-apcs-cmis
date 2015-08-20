ca762d6c35abd61bd4f35ed06db8eb3650cc5f60
cmis <cmis@cmis-hs10.(none)>
2015-08-20 14:33:34 +0700

    add fun1 and 2

---
+++ FundamentalsP1/FundamentalsP1.java
@@ -0,0 +1,13 @@
+public class FundamentalsP1
+{
+     public static void main ( String args[] )
+  {
+    System.out.println("Favorites");
+    
+    System.out.println("Food: Lamb chob");
+    System.out.println("Color: Blue");
+    System.out.println("Movie: Mean Girls ");
+    System.out.println("Musical Group: EXO");
+  } 
+} 
+
---
+++ FundamentalsP2/Fundamentals.java
@@ -0,0 +1,24 @@
+
+import javax.swing.JOptionPane;
+
+public class Fundamentals
+ {
+    public static void main(String[] args)
+    {
+            
+           String a = JOptionPane.showInputDialog ( "Give me a number");
+           String b = JOptionPane.showInputDialog ( "Give me another number");
+           
+        if( a > b )
+            { 
+           System.out.println( "Hello dude" );
+            }
+        else
+             {
+           System.out.println( "Hello,"+a );
+            }
+            
+            
+        }     
+    }     
+            
