5256484da0025e05707bbd7fb2a23371a130f647
cmis <cmis@cmis-hs10.(none)>
2015-08-21 13:11:45 +0700

    Finished P2 and add P3

--- FundamentalsP2/Fundamentals.java
+++ FundamentalsP2/Fundamentals.java
@@ -6,19 +6,23 @@ public class Fundamentals
     public static void main(String[] args)
     {
             
-           String a = JOptionPane.showInputDialog ( "Give me a number");
-           String b = JOptionPane.showInputDialog ( "Give me another number");
+           String na = JOptionPane.showInputDialog ( "Give me a number");
+           String nb = JOptionPane.showInputDialog ( "Give me another number");
+           int a=Integer.parseInt(na);
+           int b=Integer.parseInt(nb);
            
         if( a > b )
             { 
-           System.out.println( "Hello dude" );
+           System.out.println( "The value of a is " +a+ " and it is greater than b whose value is " +b +"." );
             }
-        else
+        else if (a < b )
              {
-           System.out.println( "Hello,"+a );
+           System.out.println( "The value of a is " +a+ " and it is less than b whose value is " +b+ "." );
             }
-            
-            
+         else 
+             {
+           System.out.println( "The value of a is " +a+ " and it is equal than b whose value is " +b+ "." );
+             }           
         }     
     }     
             
