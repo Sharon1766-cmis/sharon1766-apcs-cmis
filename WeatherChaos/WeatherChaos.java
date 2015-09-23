92f24f4adb745be59e7782da286faf006920b91f
cmis <cmis@cmis-hs10.(none)>
2015-09-23 12:02:21 +0700

    T__T

--- WeatherChaos/WeatherChaos.java
+++ WeatherChaos/WeatherChaos.java
@@ -5,6 +5,7 @@ public class WeatherChaos
 public static void main ( String[] argv )
     {
         String nInputdays = JOptionPane.showInputDialog ("How Many Days?");
+     
         
         int Inputdays = Integer.parseInt(nInputdays);
         
@@ -13,21 +14,52 @@ public static void main ( String[] argv )
         int PreTemp = 0;
         int day;
         int swing;
+        String des;
+       
  
         
         for ( int i = 0; i < Inputdays; i++ )
         {
             Temp = (int) (Math.random() * 200) - 100 ;
             day = i +1 ;
-            swing = PreTemp;
+            swing = PreTemp-Temp;
+            des = " ";
+            
+   
+              if (Temp < 0)  
+              {
+                  des = "freezing";
+                  
+              }
+              
+              if (Temp >0 && Temp<15) ;
+              {
+                  des =  "CHilly" ;
+                    
+              }
+              
+               if (Temp > 15 && Temp<30) ;
+              {
+                  des =  "comfortable" ;
+                    
+              }
+              
+               if (Temp > 31 && Temp<40) ;
+              {
+                  des =  "hot" ;
+            
+              }
+              
+                if (Temp >= 40)  
+              {
+                  des =  "AAAHHHHHH" ;
+                   
+              }
+            
+              System.out.println(day + "   "+ Temp + "   " + "   "+swing + "   " +des);
            
-           
-               System.out.println(day + "   "+Temp+ "   " +swing);
-               
-            PreTemp = Temp;
-          
-        }
-        
         
         }   
+       
     }
+}
