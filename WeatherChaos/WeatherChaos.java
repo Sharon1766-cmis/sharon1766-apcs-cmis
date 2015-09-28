08b5f0f20f616ec542251f6c3c3bbed91e0b4c91
cmis <cmis@cmis-hs09.(none)>
2015-09-28 09:45:56 +0700

    grading

--- WeatherChaos/WeatherChaos.java
+++ WeatherChaos/WeatherChaos.java
@@ -2,13 +2,11 @@ import javax.swing.JOptionPane;
 
 public class WeatherChaos
 {
-public static void main ( String[] argv )
+    public static void main ( String[] argv )
     {
         String nInputdays = JOptionPane.showInputDialog ("How Many Days?");
-       
-        
+
         int Inputdays = Integer.parseInt(nInputdays);
-        
         int [] days = new int [Inputdays];
         int Temp;
         int PreTemp = 0;
@@ -20,75 +18,76 @@ public static void main ( String[] argv )
         int MinDay = 0;
         int MaxDay = 0;
         int sum = 0;
-        
-        if ( Inputdays <= 0 && Inputdays >= 32)
+
+        if( Inputdays >= 1 && Inputdays <= 31)
+        {
+
+            System.out.println("day" + "   "+" Temp" + "   " + "swing  "+"   " +"des");
+
+            for ( int i = 0; i < Inputdays; i++ )
             {
-                  System.out.println("NOT A VALID DAY");
-            }
-      else 
-            {
-              
-                System.out.println("day" + "   "+" Temp" + "   " + "swing  "+"   " +"des");
-                
-                for ( int i = 0; i < Inputdays; i++ )
+                Temp = (int) (Math.random() * 200) - 100 ;
+                day = i +1 ;
+                swing = PreTemp-(-Temp);
+                des = " ";
+
+                if ( Temp < min )
                 {
-                    Temp = (int) (Math.random() * 200) - 100 ;
-                    day = i +1 ;
-                    swing = PreTemp-(-Temp);
-                    des = " ";
-                    
-                    if ( Temp < min )
-                    {
-                        min = Temp;
-                        MinDay = day;
-                    }
-                    
-                    if ( Temp > max )
-                    {
-                        max = Temp;
-                        MaxDay = day;
-                    }
-           
-                      if (Temp < 0)  
-                      {
-                          des = "freezing";
-                          
-                      }
-                      
-                      if (0<Temp &&Temp<15) 
-                      {
-                          des =  "CHilly" ;
-                            
-                      }
-                      
-                       if ( 15 < Temp && Temp<30) 
-                      {
-                          des =  "comfortable" ;
-                            
-                      }
-                      
-                       if (31 < Temp && Temp<40) 
-                      {
-                          des =  "hot" ;
-                    
-                      }
-                      
-                        if (Temp >= 40)  
-                      {
-                          des =  "AAAHHHHHH" ;
-                           
-                      }
-                    
-                      System.out.println(day + "         "+ Temp + "       " + "      "+swing + "       " +des);
-                      
-                    sum +=Temp;
-                
-                }   
-                    System.out.println("The minimum temperature was " + min + " on day " + MinDay + ".");
-                    System.out.println("The maximum temperature was " + max +"on day" + MaxDay + ".");
-                    System.out.println("The average temperature was "+ (double)sum/(double)Inputdays + ".");
-                    System.out.println("BIGGEST SWING WAS .......I dont konw");
-                    
-            }
-}
+                    min = Temp;
+                    MinDay = day;
+                }
+
+                if ( Temp > max )
+                {
+                    max = Temp;
+                    MaxDay = day;
+                }
+
+                if (Temp < 0)  
+                {
+                    des = "freezing";
+
+                }
+
+                if (0<Temp &&Temp<15) 
+                {
+                    des =  "CHilly" ;
+
+                }
+
+                if ( 15 < Temp && Temp<30) 
+                {
+                    des =  "comfortable" ;
+
+                }
+
+                if (31 < Temp && Temp<40) 
+                {
+                    des =  "hot" ;
+
+                }
+
+                if (Temp >= 40)  
+                {
+                    des =  "AAAHHHHHH" ;
+
+                }
+
+                System.out.println(day + "          "+ Temp + "        " + "       "+swing + "        " +des);
+
+                sum +=Temp;
+
+            }   
+            System.out.println("The minimum temperature was " + min + " on day " + MinDay + ".");
+            System.out.println("The maximum temperature was " + max +"on day" + MaxDay + ".");
+            System.out.println("The average temperature was "+ (double)sum/(double)Inputdays + ".");
+            System.out.println("BIGGEST SWING WAS .......I dont konw");
+
+        }
+        else
+        {
+            System.out.println("NOT A VALID DAY");
+        }
+
+    }
 }
---
+++ WeatherChaosPeer/WeatherChaosPeer.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+public class WeatherChaosPeer
+
+{
+public static void main ( String[] argv )
+    {
+        String nInputdays = JOptionPane.showInputDialog ("How Many Days?");
+       
+        
+        int Inputdays = Integer.parseInt(nInputdays);
+        
+        int [] days = new int [Inputdays];
+        int Temp;
+        int PreTemp = 0;
+        int day;
+        int swing;
+        String des;
+        int min = 99;
+        int max = 0;
+        int MinDay = 0;
+        int MaxDay = 0;
+        int sum = 0;
+                    
+    }
+}
