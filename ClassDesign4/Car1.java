41490d898b34e92328e644d066ccffa7f91ce888
cmis <cmis@cmis-hs10.(none)>
2015-10-13 14:11:09 +0700

    now on 5

---
+++ ClassDesign4/Car1.java
@@ -0,0 +1,29 @@
+
+public class Car1
+{
+ 
+   private String  Name;
+   private int Seating;
+   
+public Car1()
+    {
+        Name = new String ("Ford Fiesta");
+        Seating = 5;
+       
+    }    
+public Car1 (String Name, int Seating )    
+    {
+       this.Name = Name;
+       this.Seating = Seating;
+      
+    }   
+public String toString()
+    {
+        String output = new String ();
+        output = "Name:"+Name +"\n"+
+                        "Seating:" +Seating+"\n" ;
+             
+        return output;
+    } 
+    
+}
---
+++ ClassDesign4/CarDriver.java
@@ -0,0 +1,14 @@
+
+public class CarDriver
+{
+ public static void main ( String[] args )
+ 
+    {
+        Car1 Car1=new Car1();
+        Car1 Car2 = new Car1(  "Ferrari" , 2 );
+        
+        System.out.println(Car1);
+        System.out.println( Car2);
+        
+    }
+}
