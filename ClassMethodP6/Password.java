e261cf40bff289a6f4ff771d7d0d534c5f737840
Sharon1766 <sharon1766@cmis.ac.th>
2015-12-10 20:54:54 +0700

    fixed password

--- ClassMethodP6/Password.java
+++ ClassMethodP6/Password.java
@@ -1,26 +1,29 @@
 import javax.swing.JOptionPane;
-public class Password
+
+public class Password 
 {
-    public static void main(String[] args)
-    {
-         int counts =0;
-        String password=new String(JOptionPane.showInputDialog("Make a passwords with length of at least 6 characters and at least one non-alphanumeric character (+, -, *, /, or @.)"));
-        
-       
-      for(int i=0;i<password.length();i++)
+      public static void main(String args[]) 
       {
-            if(password.substring(i).equals("+")||password.substring(i).equals("-")||password.substring(i).equals("*")||password.substring(i).equals("/")||password.substring(i).equals("@"))
-        {
-            counts+=1;// counts the entire length of password until reachs a speical character
-        }
-      }
-      if (password.length()<6 || counts == 0)
-     {
-        System.out.println("Accepted!");
-     }
-     else
-     {
-        System.out.println("Failed!");
-     }
-    }
+          int counts = 0;
+          String Password = JOptionPane.showInputDialog("Create a passwords with a length of at least 6 characters and at least one non-alphanumeric character (+, -, *, /, or @.)");
+          
+          for(int i = 0; i < Password.length(); i++)
+          {
+              
+              if (Password.substring(i, i+1).equals("+") || Password.substring(i, i+1).equals("-") || Password.substring(i, i+1).equals("*") || Password.substring(i, i+1).equals("/") || Password.substring(i, i+1).equals("@"))
+              {
+                  counts += 1;
+                }
+            }
+          
+          if (Password.length() >= 6 && counts == 1)
+              {
+                  System.out.println("Accepted");
+                }
+          else
+              {
+                  System.out.println("Failed");
+                }
+          
+    }   
 }
